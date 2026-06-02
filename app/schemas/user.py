fcc6290c821dc4867239eab78f98f49e7864bdde
Ali Jandaghi <jandaghi14@gmail.com>
2026-06-02 16:37:18 +0330

    feat: add User schemas with UserUpdate

---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, ConfigDict
+from app.core.enums import Role
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserRegister(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    id: int
+    role: Role
+    is_active: bool
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    role: Optional[Role] = None
+    is_active: Optional[bool] = None
+    password: Optional[str] = None
