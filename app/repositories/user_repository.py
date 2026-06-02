f4bee9026338c3b37388d64568ee9a4b41ed1352
Ali Jandaghi <jandaghi14@gmail.com>
2026-06-02 16:40:47 +0330

    feat: add UserRepository with async CRUD and get_by_username

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,60 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update
+from app.models.user import User
+from app.repositories.base_repository import BaseRepository
+from app.schemas import user
+
+
+class UserRepository(BaseRepository):
+
+    async def create(self,
+                     data: user.UserRegister,
+                     session: AsyncSession
+                     ):
+        new_user = User(**data.model_dump())
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
+
+    async def get_by_id(self,
+                        id: int,
+                        session: AsyncSession):
+        result = await session.execute(select(User).where(User.id == id))
+        return result.scalars().first()
+
+    async def get_all(self, session: AsyncSession):
+        result = await session.execute(select(User))
+        return result.scalars().all()
+
+    async def update(self,
+                     id: int,
+                     session: AsyncSession,
+                     data: user.UserUpdate
+                     ):
+        await session.execute(
+            update(User)
+            .where(User.id == id)
+            .values(**data.model_dump(exclude_unset=True))
+        )
+        await session.commit()
+        return True
+
+    async def delete(self,
+                     id: int,
+                     session: AsyncSession):
+        result = await session.execute(select(User).where(User.id == id))
+        user = result.scalars().first()
+        if user:
+            await session.delete(user)
+            await session.commit()
+            return True
+        else:
+            return False
+
+    async def get_by_username(self,
+                              username: str,
+                              session: AsyncSession
+                              ):
+        result = await session.execute(select(User).where(User.username == username))
+        return result.scalars().first()
